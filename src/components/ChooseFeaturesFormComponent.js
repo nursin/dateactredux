16799504469da4813383b6cdc4484849b298a050
Bobby Keel <bobbykeel76@gmail.com>
2021-07-25 16:36:38 -0400

    redirect fix

--- src/components/ChooseFeaturesFormComponent.js
+++ src/components/ChooseFeaturesFormComponent.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { Card, CardHeader, CardBody, Label, Button } from 'reactstrap';
-import { Redirect } from 'react-router-dom';
-import { Control, LocalForm} from 'react-redux-form';
+import { Redirect, Link } from 'react-router-dom';
+import { Control, LocalForm } from 'react-redux-form';
 import { selectedChooseFormValues } from '../redux/ActionCreators';
 import { connect } from 'react-redux';
+import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
 
 const mapStateToProps = state => {
   return {
@@ -26,7 +27,7 @@ class RenderChooseFeaturesForm extends Component {
 
   }
 
-  handleSubmit (values) {
+  handleSubmit(values) {
     // console.log("Current state is: " + JSON.stringify(values));
     // console.log("Gender:", values.charGender)
     // console.log("Age:", values.charAge) 
@@ -44,7 +45,7 @@ class RenderChooseFeaturesForm extends Component {
         changePage: false
       })
       return <Redirect to='/home/createchar' />
-    } else if (this.state.changePage){
+    } else if (this.state.changePage) {
       this.setState({
         changePage: false
       })
@@ -55,8 +56,12 @@ class RenderChooseFeaturesForm extends Component {
   render() {
     return (
       <div className='container'>
-        <div className='row row-content'>
+        <div className='row mb-5'>
           <div className='col-12'>
+            <Breadcrumb>
+              <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+              <BreadcrumbItem active>Choose character features</BreadcrumbItem>
+            </Breadcrumb>
             <Card className='custom-card'>
               <CardHeader className='bg-gradient' >
                 <h2 className='text-white text-center'>Choose character features</h2>
@@ -65,42 +70,42 @@ class RenderChooseFeaturesForm extends Component {
                 <LocalForm onSubmit={values => this.handleSubmit(values)}>
                   <div className='form-group'>
                     <Label htmlFor='charGender'>Gender</Label>
-                      {/* eslint-disable-next-line */}
-                      <Control.select id='charGender' model='.charGender' className='form-control' name='charGender'>
-                        <option default>Select...</option>
-                        <option value='male'>Male</option>
-                        <option value='female'>Female</option>
-                        <option value='androgen'>Androgyne</option>
-                        <option value='neutral'>Gender Nonconforming</option>
-                      </Control.select>
+                    {/* eslint-disable-next-line */}
+                    <Control.select id='charGender' model='.charGender' className='form-control' name='charGender'>
+                      <option default>Select...</option>
+                      <option value='male'>Male</option>
+                      <option value='female'>Female</option>
+                      <option value='androgen'>Androgyne</option>
+                      <option value='neutral'>Gender Nonconforming</option>
+                    </Control.select>
                   </div>
                   <div className='form-group'>
                     <Label htmlFor='charAge'>Age range</Label>
                     {/* eslint-disable-next-line */}
-                      <Control.select id='charAge' model='.charAge' className='form-control' name='charAge'>
-                        <option default>Select...</option>
-                        <option value='10-19'>10 - 19</option>
-                        <option value='20-29'>20 - 29</option>
-                        <option value='30-39'>30 - 39</option>
-                        <option value='40-49'>40 - 49</option>
-                        <option value='50-59'>50 - 59</option>
-                        <option value='59+'>59+</option>
-                      </Control.select>
+                    <Control.select id='charAge' model='.charAge' className='form-control' name='charAge'>
+                      <option default>Select...</option>
+                      <option value='10-19'>10 - 19</option>
+                      <option value='20-29'>20 - 29</option>
+                      <option value='30-39'>30 - 39</option>
+                      <option value='40-49'>40 - 49</option>
+                      <option value='50-59'>50 - 59</option>
+                      <option value='59+'>59+</option>
+                    </Control.select>
                   </div>
                   <div className='form-group'>
                     <Label htmlFor='charType'>Type</Label>
                     {/* eslint-disable-next-line */}
-                      <Control.select id='charType' model='.charType' className='form-control' name='charType'>
-                        <option default>Select...</option>
-                        <option value='famous'>Famous</option>
-                        <option value='notFamous'>Not famous</option>
-                        <option value='createYourOwn'>Create your own</option>
-                      </Control.select>
+                    <Control.select id='charType' model='.charType' className='form-control' name='charType'>
+                      <option default>Select...</option>
+                      <option value='famous'>Famous</option>
+                      <option value='notFamous'>Not famous</option>
+                      <option value='createYourOwn'>Create your own</option>
+                    </Control.select>
                   </div>
                   <div className='form-group mt-4'>
-                      <Button type='submit' className='col btn btn-lg text-white bg-blue button-hover-bg-gradient'>
-                        <i className='fa fa-transgender-alt fa-lg' /> Generate
-                      </Button>
+                    <Button type='submit' className='col btn btn-lg text-white bg-blue button-hover-bg-gradient'>
+                      <i className='fa fa-transgender-alt fa-lg' /> Generate
+                    </Button>
                   </div>
                 </LocalForm>
               </CardBody>
